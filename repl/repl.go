c2ee9848881d9e9e624cc9da8add006c8b0f0677
y_fukuda <colors92window@gmail.com>
2020-07-26 23:52:42 +0900

    prototype repl only with lexer

---
+++ repl/repl.go
@@ -0,0 +1,29 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey-study/lexer"
+	"monkey-study/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok) // the plus flag adds field names
+		}
+	}
+}
