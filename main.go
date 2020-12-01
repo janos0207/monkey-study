bb2af710bbfd43549da601ad79fe8c3f576307d3
y_fukuda <colors92window@gmail.com>
2020-12-01 23:17:57 +0900

    update repl with parser

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 	"os/user"
 )
 
+const MONKEY_LOGO = `    __  ___            __
+   /  |/  /___  ____  / /_____  __  __
+  / /|_/ / __ \/ __ \/ //_/ _ \/ / / /
+ / /  / / /_/ / / / / ,< /  __/ /_/ /
+/_/  /_/\____/_/ /_/_/|_|\___/\__, /
+                             /____/
+`
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(MONKEY_LOGO)
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Name)
 	fmt.Printf("Feel free to type in commands!\n")
 	repl.Start(os.Stdin, os.Stdout)
