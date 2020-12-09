57279f78c6f0f0ee50a67a3331dfd45c361452b3
y_fukuda <colors92window@gmail.com>
2020-12-09 11:50:45 +0900

    evaluate bang operator expression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -74,3 +74,24 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 
 	return true
 }
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!0", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+		{"!!0", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
